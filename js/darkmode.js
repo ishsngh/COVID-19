78f832a1b62f248c74c6f659f3f85fa023caae16
Ishaan Singh <46941241+ishsngh@users.noreply.github.com>
2021-05-14 20:43:52 +0530

    Add files via upload

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,7 +1,18 @@
+var lightmode = document.getElementById('inner-switch');
+
 $( ".inner-switch" ).on("click", function() {
     if( $( "body" ).hasClass( "dark" )) {
       $( "body" ).removeClass( "dark" );
+      $( "html" ).removeClass( "dark" );
+      localStorage.setItem('lightmode', 'true');
     } else {
       $( "body" ).addClass( "dark" );
+      $( "html" ).addClass( "dark" );
+      localStorage.setItem('lightmode', 'false');
     }
 });
+
+if (localStorage.getItem('lightmode') === 'true') {
+	  $( "body" ).removeClass( "dark" );
+      $( "html" ).removeClass( "dark" );
+}
